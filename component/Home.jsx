5efc8e2d5d54d2b187ff5b102424f809ac075910
nizeyimanadanny <nizeyimanadanny44@gmail.com>
2024-08-07 09:18:20 +0200

    Add files via upload

---
+++ component/Home.jsx
@@ -0,0 +1,15 @@
+function Home(){
+    return(
+<div className="">
+    <h1>
+  YOU ARE WELCOME ON OUR HOME PAGE <br/>
+    </h1>
+   < marquee >DANNY AND ALPHA   </marquee>
+
+</div>
+    )
+}
+export default Home;
+    
+  
+   
